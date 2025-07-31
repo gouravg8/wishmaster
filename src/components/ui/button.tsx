773f9c77f19d6a9b21917c4805d51426918aa6c4
Gourav soni <gourav.soni@synlabs.io>
2025-07-31 17:52:51 +0530

    added loader icon in button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import { Loader2Icon } from "lucide-react"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
@@ -39,10 +40,11 @@ export interface ButtonProps
   asChild?: boolean;
   icon?: React.ReactNode;
   iconAt?: "start" | "end";
+  isLoading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, icon, iconAt = "start", ...props }, ref) => {
+  ({ className, variant, size, asChild = false, icon, iconAt = "start", isLoading = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
@@ -50,11 +52,12 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         ref={ref}
         {...props}
       >
-        {icon && iconAt === "start" && (
+        {isLoading && <Loader2Icon className="animate-spin" />}
+        {!isLoading && icon && iconAt === "start" && (
           <span className="flex items-center">{icon}</span>
         )}
         {props.children}
-        {icon && iconAt === "end" && (
+        {!isLoading && icon && iconAt === "end" && (
           <span className="flex items-center">{icon}</span>
         )}
       </Comp>
