d8e59b2d46bd626156191628d6c8476fa1a91014
Gourav soni <gourav.soni@synlabs.io>
2025-08-04 12:09:42 +0530

    hide admin from normal user

--- src/config/PrivateRoute.tsx
+++ src/config/PrivateRoute.tsx
@@ -1,12 +1,18 @@
 import { useAuth } from "@/hooks/userAuth";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 // import { AuthContext } from "../Providers/AuthProvider";
 
 const PrivateRoute = ({ children }) => {
     //   const { userDetails } = useContext(AuthContext);
+    const { pathname } = useLocation();
+    console.log({ pathname });
+
     const { user } = useAuth();
     console.log({ user });
 
+    if (pathname.startsWith("/admin") && !user?.roles?.includes("LMA_ADMIN")) {
+        return <Navigate to="/" />
+    }
 
     if (!user) {
         return <Navigate to="/" />;
